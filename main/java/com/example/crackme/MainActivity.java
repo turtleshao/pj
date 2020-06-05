a3cb64d7e4b64b7de1655d1702c2c73ab1b15bbe
turtleshao <18307130076@fudan.edu.cn>
2020-06-05 15:20:02 +0800

    can you receive?

--- main/java/com/example/crackme/MainActivity.java
+++ main/java/com/example/crackme/MainActivity.java
@@ -2,33 +2,43 @@ package com.example.crackme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.nfc.Tag;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import static com.example.crackme.R.layout.activity_main;
 
 public class MainActivity extends AppCompatActivity {
+    public native String dosth(String str);
+    public native String encrypt(String str);
+    static{
+        System.loadLibrary("crack");
+    }
 
-    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(activity_main);
-        final EditText editText = findViewById(R.id.editText);
-        Log.d(TAG,"啥u算法");
+        EditText editText = findViewById(R.id.editText);
+        final String s = editText.getText().toString();
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 /*
                 运算一下s得到一串字符串，用来在后门中和s作比较，保证两个字符串相互关联
                  */
-                final String s = editText.getText().toString();
-                Toast toast = Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG);
-                toast.show();
+                String t=dosth(s);
+                SharedPreferences sharedPreferences=getSharedPreferences("key", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor=sharedPreferences.edit();
+                editor.putString("str",t);
+                editor.apply();
+                if(false){
+                    //不能这么明显orz
+                    //总之得一直是false
+                    Toast.makeText(MainActivity.this.getApplicationContext(),"right",Toast.LENGTH_LONG).show();
+                }
+                Toast.makeText(MainActivity.this.getApplicationContext(),"wrong",Toast.LENGTH_LONG).show();
             }
         });
     }
--- main/java/com/example/crackme/mActivity.java
+++ main/java/com/example/crackme/mActivity.java
@@ -1,7 +1,9 @@
 package com.example.crackme;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
@@ -9,24 +11,26 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class mActivity extends Activity {
+    public native boolean sthIsHappening(String str1,String str2);
+    public native boolean a(String string);
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        String s = getIntent().getStringExtra("msg");
-        /*
-        填入判断函数，结合MainActivity中得到的另一个s，判断Intent中得到的信息msg输入是否符合条件，如果符合则继续，否则退出
-        此处打算用native函数判断来迷惑一下
-         */
-
+        String s=getIntent().getStringExtra("msg");
+        SharedPreferences sharedPreferences=getSharedPreferences("key", Context.MODE_PRIVATE);
+        String t=sharedPreferences.getString("str","default");
+        if(sthIsHappening(s,t)){
+            return;
+        }
         setContentView(R.layout.m_main);
-        final EditText editText= findViewById(R.id.editText2);
-
+        EditText editText= findViewById(R.id.editText2);
+        final String s1=editText.getText().toString();
         findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 /*
                 判断flag是否正确
                  */
-                String s1=editText.getText().toString();
+                a(s1);
             }
         });
     }
