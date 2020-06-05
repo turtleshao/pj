d29ce04df502f65c101b80bcd0ef48ec63165869
zmh2000829 <18307130070@fudan.edu.cn>
2020-06-05 23:52:13 +0800

    runnable and different UI

--- main/java/com/example/crackme/MainActivity.java
+++ main/java/com/example/crackme/MainActivity.java
@@ -4,42 +4,61 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import static com.example.crackme.R.layout.activity_main;
 
 public class MainActivity extends AppCompatActivity {
-    public native String dosth(String str);
-    public native String encrypt(String str);
-    static{
-        System.loadLibrary("crack");
-    }
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EditText editText = findViewById(R.id.editText);
-        final String s = editText.getText().toString();
+        setContentView(activity_main);
+        final EditText editText = findViewById(R.id.editText);
+        Log.d(TAG,"啥u算法");
+        String x = "flag";
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*
-                运算一下s得到一串字符串，用来在后门中和s作比较，保证两个字符串相互关联
-                 */
-                String t=dosth(s);
+                final String s = editText.getText().toString();
+                String t = s;
                 SharedPreferences sharedPreferences=getSharedPreferences("key", Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor=sharedPreferences.edit();
                 editor.putString("str",t);
                 editor.apply();
-                if(false){
-                    //不能这么明显orz
-                    //总之得一直是false
-                    Toast.makeText(MainActivity.this.getApplicationContext(),"right",Toast.LENGTH_LONG).show();
+//                Toast toast = Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG);
+//                toast.show();
+                if (checkflag(s)){
+                    Toast.makeText(MainActivity.this.getApplicationContext(),"Right answer",Toast.LENGTH_LONG).show();
                 }
-                Toast.makeText(MainActivity.this.getApplicationContext(),"wrong",Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this.getApplicationContext(),"wrong forever",Toast.LENGTH_LONG).show();
             }
         });
+    }
+    private boolean checkflag(String t) {
+        String a1 = "pXrZt";//4
+        String a3 = "O]QlV";//3
+        String a2 = "xTv\\Q";//2
+        String a4 = "FmCjM";//1
+
+        char[] a = t.toCharArray();
+        int sum = 0;
+        for(int i = 0; i < t.length(); i++) {
+            sum += a[i];
+            if((a[i] < 'a' && a[i] > 'Z')||a[i] <= 64)
+                return false;
+            a[i] += i;
+            System.out.println(a[i]);
+        }
+        String soeasy = a4+a2+a3+a1;
+        String s = String.valueOf(a);
+        if(sum != 1701 || !s.equals(soeasy)) return false;
+        return true;
     }
 }
--- main/java/com/example/crackme/mActivity.java
+++ main/java/com/example/crackme/mActivity.java
@@ -1,37 +1,59 @@
 package com.example.crackme;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class mActivity extends Activity {
-    public native boolean sthIsHappening(String str1,String str2);
-    public native boolean a(String string);
+    private static final String TAG = "mActivity";
+
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        String s=getIntent().getStringExtra("msg");
-        SharedPreferences sharedPreferences=getSharedPreferences("key", Context.MODE_PRIVATE);
-        String t=sharedPreferences.getString("str","default");
-        if(sthIsHappening(s,t)){
-            return;
+        String s = getIntent().getStringExtra("msg");
+        SharedPreferences sharedPreferences = getSharedPreferences("key", Context.MODE_PRIVATE);
+        String t = sharedPreferences.getString("str","");
+        if(check(s,t)){
+            setContentView(R.layout.w_main);
+            final TextView helloTextView = findViewById(R.id.textView);
+            helloTextView.setText(R.string.flag_not_here);
         }
-        setContentView(R.layout.m_main);
-        EditText editText= findViewById(R.id.editText2);
-        final String s1=editText.getText().toString();
-        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /*
-                判断flag是否正确
-                 */
-                a(s1);
-            }
-        });
+        else {
+            setContentView(R.layout.m_main);
+            final EditText editText = findViewById(R.id.editText2);
+            findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String s1 = editText.getText().toString();
+                    Toast toast = Toast.makeText(mActivity.this, s1, Toast.LENGTH_LONG);
+                    toast.show();
+                    /**/
+                }
+            });
+        }
     }
+    private boolean check(String s, String t) {
+        int i = 0, len = t.length();
+        Log.d(TAG,len+"here");
+        char[] c = t.toCharArray();
+        while (i < len - 1){
+            if(i % 2 == 1){
+                c[i] = '0';
+            }
+            i++;
+        }
+        String h = String.valueOf(c);
+        if (s.equals(h)) return false;
+        else return true;
+    }
+
 }
