f7a35ddf2affe1cf52faeb47318df1f06404ed0e
zmh2000829 <18307130070@fudan.edu.cn>
2020-06-05 15:10:13 +0800

    zmh-new

--- main/java/com/example/crackme/MainActivity.java
+++ main/java/com/example/crackme/MainActivity.java
@@ -2,24 +2,33 @@ package com.example.crackme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import static com.example.crackme.R.layout.activity_main;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EditText editText = findViewById(R.id.editText);
-        String s = editText.getText().toString();
+        setContentView(activity_main);
+        final EditText editText = findViewById(R.id.editText);
+        Log.d(TAG,"啥u算法");
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 /*
                 运算一下s得到一串字符串，用来在后门中和s作比较，保证两个字符串相互关联
                  */
+                final String s = editText.getText().toString();
+                Toast toast = Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG);
+                toast.show();
             }
         });
     }
--- main/java/com/example/crackme/mActivity.java
+++ main/java/com/example/crackme/mActivity.java
@@ -11,20 +11,22 @@ import android.widget.EditText;
 public class mActivity extends Activity {
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        String s=getIntent().getStringExtra("msg");
+        String s = getIntent().getStringExtra("msg");
         /*
         填入判断函数，结合MainActivity中得到的另一个s，判断Intent中得到的信息msg输入是否符合条件，如果符合则继续，否则退出
         此处打算用native函数判断来迷惑一下
          */
+
         setContentView(R.layout.m_main);
-        EditText editText= findViewById(R.id.editText2);
-        String s1=editText.getText().toString();
+        final EditText editText= findViewById(R.id.editText2);
+
         findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 /*
                 判断flag是否正确
                  */
+                String s1=editText.getText().toString();
             }
         });
     }
